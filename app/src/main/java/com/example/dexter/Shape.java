954a9fb627a6f49255448c422db400a4a7b49f17
Hualin457 <hualin@ualberta.ca>
2025-09-23 19:19:22 -0600

    add color property to Shape.java

--- app/src/main/java/com/example/dexter/Shape.java
+++ app/src/main/java/com/example/dexter/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
     public int x;
     public int y;
 
+    private String color = "azure";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
