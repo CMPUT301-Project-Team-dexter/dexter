bcbb8d4597813e071b2186052d56c7da921d3acd
Hualin457 <hualin@ualberta.ca>
2025-09-23 18:29:11 -0600

    create class Shape.java

---
+++ app/src/main/java/com/example/dexter/Shape.java
@@ -0,0 +1,11 @@
+package com.example.dexter;
+
+public abstract class Shape {
+    public int x;
+    public int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
